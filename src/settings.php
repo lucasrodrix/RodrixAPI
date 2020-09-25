31f87ad0b233fc568e3f699b4698070096ebaa59
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2020-09-25 19:33:18 -0300

    Realizada instalação da dependencia Illuminate e configuração do Banco de Dados

--- src/settings.php
+++ src/settings.php
@@ -15,5 +15,17 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+        
+        //DB Settings
+        'db' => [
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'rodrix_api',
+            'username'  => 'root',
+            'password'  => '',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ]
     ],
 ];
